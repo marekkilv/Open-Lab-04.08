55866d85beb6bd526c5c96a575b9f16df01c6ca8
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 20:03:44 +0100

    mam

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            var list = new List<string>();
+            foreach(string str in strings)
+            {
+               if (str.Length == 4)
+               {
+                    list.Insert(i, str);
+                 i++;
+               }
+            }
+            return list.ToArray();
         }
     }
 }
